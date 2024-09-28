5f77d58be11b7852e62b30e88bac1507d884facb
konurov <s_konurov@mail.ru>
2024-09-28 12:03:01 +0600

    my

--- les1/classes/StaffClasses.php
+++ les1/classes/StaffClasses.php
@@ -3,7 +3,7 @@ class StaffClasses{
     // Свойства класса
   public $name = "Snajar";
   public $age = 15;
-
+#
   // Методы класса
   public function sayHello() {
     echo "Hello, my name is ".$this->name." and I am ".$this->age." years old.";
