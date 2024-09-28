79fbbd3174d0263628af1d6a0139345e4d0c8df4
konurov <s_konurov@mail.ru>
2024-09-28 12:29:21 +0600

    my

--- les1/classes/StaffClasses.php
+++ les1/classes/StaffClasses.php
@@ -3,7 +3,8 @@ class StaffClasses{
     // Свойства класса
   public $name = "Snajar";
   public $age = 15;
-#
+  #----ЗАДАЧА-------#
+  #Давайте добавим проверку введенного возраста: если он от 18 до 60, то будем менять возраст на новый, а если нет - то менять не будем:
   // Методы класса
   public function sayHello() {
     echo "Hello, my name is ".$this->name." and I am ".$this->age." years old.";
